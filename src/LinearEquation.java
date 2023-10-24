8b57b23313f06a90f6dd788192342a0873630eb5
ninjastar779 <neeldebnath2@gmail.com>
2023-10-24 18:59:19 -0400

    Added comments

--- src/LinearEquation.java
+++ src/LinearEquation.java
@@ -5,12 +5,6 @@ public class LinearEquation {
     private int x2;
     private int y2;
 
-    /* Creates a LinearEquation object */
-    /*
-     * PRECONDITION: x1 and x2 are NOT equal (client programs are responsible for
-     * ensuring
-     * this precondition is not violated)
-     */
     public LinearEquation(int x1, int y1, int x2, int y2) {
         this.x1 = x1;
         this.y1 = y1;
@@ -45,35 +39,11 @@ public class LinearEquation {
         return roundedToHundredth(slope);
     }
 
-    /*
-     * Returns a String that represents the linear equation of the line through
-     * points
-     * (x1, y1) and (x2, y2) in slope-intercept (y = mx + b) form, e.g.
-     * "y = 3x + 1.5".
-     * 
-     * When generating the m value (slope), here are examples of "printable" slopes:
-     * 5, -5, 1/2, 6/8 (reducing not required), 8/5, -2/3, -18/7
-     * 
-     * Here are non-examples of "printable" slopes:
-     * 1/-2 (should be -1/2), -4/-3 (should be 4/3), 8/4 (should be reduced to 2),
-     * -12/3 (should be -4), 3/3 (should be 1), -6/6 (should be -1)
-     * 
-     * HINT: Be sure to check if the line is horizontal and return an appropriate
-     * string,
-     * e.g. y = 6
-     * HINT: Absolute value might be helpful for ensuring proper placement of
-     * negative sign!
-     * 
-     * 
-     * When generating the b value, here are some examples of "printable"
-     * y-intercepts:
-     * + 1.0 - 2.35 + 12.5 - 8.0 + 17.19
-     * 
-     * Here are non-examples of "printable" y-intercepts:
-     * - -1.0 + -2.35 - -12.5 + -8.0 - -17.19 + 0 - 0
-     * 
-     * HINT: Absolute value might be helpful for printing negative y-intercepts as
-     * subtraction!
+    /**
+     * Generates the equation of a line based on the slope and y-intercept.
+     *
+     * @return The equation of the line in the form "y = mx + b", where m is the
+     *         slope and b is the y-intercept.
      */
     public String equation() {
         String equation = "y = ";
@@ -116,43 +86,33 @@ public class LinearEquation {
         return equation;
     }
 
-    /*
-     * Returns a String of the coordinate point on the line that has the given x
-     * value, with
-     * both x and y coordinates as decimals to the nearest hundredth, e.g (-5.0,
-     * 6.75)
+    /**
+     * Generates a coordinate in the form (x, y) for a given x value.
+     *
+     * @param  xValue  the x value for which the coordinate is generated
+     * @return         the coordinate in the form (x, y)
      */
     public String coordinateForX(double xValue) {
         String coordinate = "(" + xValue + ", " + roundedToHundredth(slope() * xValue + yIntercept()) + ")";
         return coordinate;
     }
 
-    /*
-     * "Helper" method for use elsewhere in your methods; returns the value toRound
-     * rounded
-     * to the nearest hundredth
-     * 
-     * HINT: the Math.round method can help with this!
+    /**
+     * Returns the given number rounded to the nearest hundredth.
+     *
+     * @param  toRound  the number to be rounded
+     * @return          the rounded number
      */
     public double roundedToHundredth(double toRound) {
         double rounded = Math.round(toRound * 100);
         return rounded / 100;
     }
 
-    /*
-     * Returns a string that includes all information about the linear equation,
-     * each on
-     * separate lines:
-     * - The original points: (x1, y1) and (x2, y2)
-     * - The equation of the line in y = mx + b format (using equation() method)
-     * - The slope of the line, as a decimal (using slope() method)
-     * - The y-intercept of the line (using yIntercept() method)
-     * - The distance between the two points (using distance() method)
-     * 
-     * This method should call all other appropriate methods to get the info it
-     * needs:
-     * equation(), slope(), yIntercept(), distance().
-     * 
+    /**
+     * Returns a string containing information about the line.
+     *
+     * @return          a string with the original points, the equation of the line, the slope,
+     *                  the y-intercept, and the distance between the two points
      */
     public String lineInfo() {
         String info = "The original points: (" + x1 + ", " + y1 + ") and (" + x2 + ", " + y2 + ")\n" +
