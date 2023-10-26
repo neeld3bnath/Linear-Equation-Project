e2948821d1e70039d463e97cefe0be7fb1392ab3
Neel Debnath <neeld3@nycstudents.net>
2023-10-26 08:45:24 -0400

    Fixed class name

--- src/LinearEquationRunner.java
+++ src/LinearEquationRunner.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-class Main {
+class LinearEquationRunner {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
         System.out.println("Welcome!");
